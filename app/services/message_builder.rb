efc76ee2532ec1dbe67988960389dc5f21037eb4
Somsak Arnon <lahphim@gmail.com>
2017-12-22 11:45:25 +0700

    Improve MessageBuilder.

--- app/services/message_builder.rb
+++ app/services/message_builder.rb
@@ -1,4 +1,37 @@
 class MessageBuilder
+  attr_accessor :conversation
+
+  def initialize
+     @conversation = {
+       travel: {
+         reply: [
+           'So exciting!!! Where would you like to go?',
+           'Where would you like to go?',
+           'What\'s your favorite place go?',
+           'Just tell me :)'
+         ],
+         end_point: [
+           '/hotel_location'
+         ]
+       },
+       go: {
+         reply: [
+           'Look!! That\'s such a nice place!!!',
+           'Come on!! Let\'s make it happen today :)',
+           'This is the best hotel *EVER* :)'
+         ],
+         end_point: %w[/hotel_location /hotel/location]
+       }
+     }.freeze
+  end
+
+  def built_text
+    {
+      type: 'text',
+      text: conversation[key][:reply].sample
+    }
+  end
+
   def built_sticker
     {
       type: 'sticker',
